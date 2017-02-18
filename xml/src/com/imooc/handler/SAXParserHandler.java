e95739b6b7fcf031fc2476daf67b7e818f189825
nilzxq <2013358717@qq.com>
2017-02-18 16:12:14 +0800

    解析属性

--- xml/src/com/imooc/handler/SAXParserHandler.java
+++ xml/src/com/imooc/handler/SAXParserHandler.java
@@ -10,6 +10,7 @@ import org.xml.sax.helpers.DefaultHandler;
  * @version 1.0
  */
 public class SAXParserHandler extends DefaultHandler {
+	int bookIndex=0;
 	/*
 	 * 用来遍历xml文件的开始标签
 	 */
@@ -18,8 +19,10 @@ public class SAXParserHandler extends DefaultHandler {
 			Attributes attributes) throws SAXException {
       //调用DefaultHanlder类的startElement方法
 		super.startElement(uri, localName, qName, attributes);
-		//开始解析book元素的属性
 		if(qName.equals("book")){
+			bookIndex++;
+			//开始解析book元素的属性
+			System.out.println("==============下面开始遍历第"+bookIndex+"本书的内容==============");
 //			//已知book元素下的属性名称，根据属性名称或取属性值
 //			String value=attributes.getValue("id");
 //			System.out.println("book的属性值是："+value);
@@ -37,8 +40,11 @@ public class SAXParserHandler extends DefaultHandler {
 	@Override
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException {
-		// TODO Auto-generated method stub
+		//调用DefaultHanlder类的endElement方法
 		super.endElement(uri, localName, qName);
+		if(qName.equals("book"))
+		System.out.println("==============结束遍历第"+bookIndex+"本书的内容==============");
+		
 	}
    
 	/*
