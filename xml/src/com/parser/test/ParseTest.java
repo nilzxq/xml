25756f22c58d1bc4b8d4c03270ae50740f0cceea
nilzxq <2013358717@qq.com>
2017-02-19 19:13:04 +0800

    junit test

--- xml/src/com/parser/test/ParseTest.java
+++ xml/src/com/parser/test/ParseTest.java
@@ -63,6 +63,8 @@ public class ParseTest {
 						else if (name.equals("author")) {
 							bookEntity.setAuthor(value);
 						}
+						
+						
 						else if (name.equals("year")) {
 							bookEntity.setYear(value);
 						}
