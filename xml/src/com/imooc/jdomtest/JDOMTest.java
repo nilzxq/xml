5ddc6f85f65a40138e27d42b1249efd321048a1e
nilzxq <2013358717@qq.com>
2017-02-19 17:31:17 +0800

    jdom attribute

--- xml/src/com/imooc/jdomtest/JDOMTest.java
+++ xml/src/com/imooc/jdomtest/JDOMTest.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -33,6 +34,23 @@ public class JDOMTest {
 		      Element rootElement = document.getRootElement();
 //    	  5.获取根结点下的子节点的List集合
 		      List<Element> bookList = rootElement.getChildren();
+		      //继续遍历
+		      for (Element book : bookList) {
+				System.out.println("====开始解析第"+(bookList.indexOf(book)+1)+"本书");
+				//解析book的属性集合
+				List<Attribute> attrList = book.getAttributes();
+//				//已知节点下的属性名值，获得节点名
+//				book.getAttribute("id");
+				//遍历attrList(针对不清楚book节点下属性的名字及数量)
+				for (Attribute attr : attrList) {
+					//获取属性名
+					String attrName = attr.getName();
+					//获取属性值
+					String attrValue=attr.getValue();
+					System.out.println("属性名："+attrName+"------属性值："+attrValue);
+				}
+				System.out.println("====结束解析第"+(bookList.indexOf(book)+1)+"本书");
+			}
 			} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}catch (JDOMException e) {
